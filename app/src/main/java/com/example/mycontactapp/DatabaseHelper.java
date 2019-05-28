6261a08c6a7cd96a2c4a226a6fd2f9a9c5f0d3b3
Aniket Pratap <PratapA1028@sduhsd.lan>
2019-05-28 10:59:05 -0700

    SQL

---
+++ app/src/main/java/com/example/mycontactapp/DatabaseHelper.java
@@ -0,0 +1,35 @@
+package com.example.mycontactapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_Version = 1;
+    public static final String DATABASE_Name = "Contact2-2019.db";
+    public static final String TABLE_Name = "Contact2-2019_table";
+    public static final String ID = "ID";
+    public static final String COLUMN_NAME_CONTACT = "contact";
+
+
+    public static final String SQL_CREATE_ENTERIES =
+            "CREATE TABLE" + TABLE_Name + " (" +
+                    ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    COLUMN_NAME_CONTACT + " TEXT)";
+
+
+    public DatabaseHelper(@androidx.annotation.Nullable Context context, @androidx.annotation.Nullable String name, @androidx.annotation.Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
