e8567c4a02c84088ea54d6fff945cfe683dd2136
Aniket Pratap <PratapA1028@sduhsd.lan>
2019-05-29 11:00:30 -0700

    despacito

--- app/src/main/java/com/example/mycontactapp/DatabaseHelper.java
+++ app/src/main/java/com/example/mycontactapp/DatabaseHelper.java
@@ -1,8 +1,10 @@
 package com.example.mycontactapp;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
@@ -14,22 +16,64 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 
     public static final String SQL_CREATE_ENTERIES =
-            "CREATE TABLE" + TABLE_Name + " (" +
+            "CREATE TABLE " + TABLE_Name + " (" +
                     ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                     COLUMN_NAME_CONTACT + " TEXT)";
 
+    public static final String SQL_DELETE_ENTRIES =
+        "DROP TABLE IF EXISTS " + TABLE_NAME;
 
     public DatabaseHelper(Context context) {
-        super(context, DATABASE_Name, factory : null, DATABASE_Version);
+        super(context, DATABASE_Name,null, DATABASE_Version);
+        Log.d("MyContactApp", "DatabaseHelper: constructed DatabaseHelper" );
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTERIES);
+        Log.d("MyContactApp", "DatabaseHelper: created database" );
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL(SQL_CREATE_ENTERIES);
+        Log.d("MyContactApp", "DatabaseHelper: upgraded database " );
+        onCreate(db);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public Cursor getAllData()
+    {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_Name, null );
+        return res;
+    }
+
 }
--- app/src/main/java/com/example/mycontactapp/MainActivity.java
+++ app/src/main/java/com/example/mycontactapp/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.mycontactapp;
 
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MyContactApp", "MainActivity: setting up the layout");
+        setContentView(R.layout.activity_main);
     }
+
+
+
+
+
+
+    public void  viewData(View view)
+    {
+        Cursor res = myDb.getAllData();
+
+        //put log.d's here
+        if(res.getCount() == 0)
+        {
+            showMessage("Error", "No Data found in database");
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        while(res.moveToNext())
+        {
+            //Append res columns to the buffer - see the StringBuffer and Cursrs's API's
+            buffer.append("ID: " + res.getString(0) + "\n");
+            buffer.append("Name: " + res.getString(1) + "\n");
+            buffer.append("Phone: " + res.getString(2) + "\n");
+            buffer.append("Address: " + res.getString(3) + "\n");
+        }
+
+        showMessage("Data", buffer.toString());
+    }
+
+
+
+
+
+public void showMessage(String title, String message)
+{
+    //put log d
+
+    AlertDialog.Builder builder = new AlertDialog.Builder((this));
+    builder.setCancelable((true));
+    builder.setTitle(title);
+}
 }
